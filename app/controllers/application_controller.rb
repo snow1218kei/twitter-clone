becf5f7d3e18e13c477e1f84f3adcb22489e4b2c
yuuki.tsujimura <snow1218kei@gmail.com>
2020-09-06 12:45:31 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,8 @@ private
   
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
   
 end
