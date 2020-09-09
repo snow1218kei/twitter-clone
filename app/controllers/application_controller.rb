53a9bb5d20c03a82dfad37dc4abe626cab6b512b
yuuki.tsujimura <snow1218kei@gmail.com>
2020-09-09 15:44:17 +0000

    micropost-complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,7 @@ private
     @count_microposts = user.microposts.count
     @count_followings = user.followings.count
     @count_followers = user.followers.count
+    @count_likes = user.likes.count
   end
   
 end
